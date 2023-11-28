f7b54d1728fb3d90d9cd17f99d22cd880a619982
RikOrlov <golubitskiy2013@mail.ru>
2023-11-28 23:51:14 +0300

    Update independent_PID_with_Ramp.ino
    Полностью переписан принцип работы генератора рампы. Теперь есть отдельная функция, которая задает скорость, прибавляя к ней ускорение до максимальной скорости и сбавляет её с учетом общего заданного расстояния.
    Запись новых значений в задающую скорость теперь работает через прерывания по таймеру, которое в дальнейшем нужно будет отрегулировать.
    Также добавлена функция включения/отключения таймера на будущее, потому что в функции прерывания у нас изменяется скорость, что не хотелось бы выполнять постоянно.

--- main/independent_PID_with_Ramp/independent_PID_with_Ramp.ino
+++ main/independent_PID_with_Ramp/independent_PID_with_Ramp.ino
@@ -1,9 +1,9 @@
 #define LEFT_SPEED_PULSE 2
 #define RIGHT_SPEED_PULSE 3
-#define LEFT_AN 5 // PWM pin
-#define RIGHT_AN 6 // PWM pin
-#define LEFT_CW 7 // HIGH - forward
-#define RIGHT_CW 8 // LOW - forward
+#define LEFT_AN 5   // PWM pin
+#define RIGHT_AN 6  // PWM pin
+#define LEFT_CW 7   // HIGH - forward
+#define RIGHT_CW 8  // LOW - forward
 
 
 int gLeftTask = 0;
@@ -25,51 +25,61 @@ float control_right;
 uint32_t given_speed_right;
 uint32_t current_speed_right;
 
-float kp_left=5.1;
-float kd_left=0.0001;
-float ki_left=0.000001;
+float kp_left = 5.1;
+float kd_left = 0.0001;
+float ki_left = 0.000001;
 uint32_t kob_left = 258;
 
-float kp_right=5.1;
-float kd_right=0.0001;
-float ki_right=0.000001;
+float kp_right = 5.1;
+float kd_right = 0.0001;
+float ki_right = 0.000001;
 uint32_t kob_right = 261;
 
-float* RampGenerator (float S, float Vmax, float E,float dt){
 
-  int32_t kob_right = 261;
-  S = S*kob_right;
-  float tnas = Vmax/E;
-  float s_nas1=S-E*tnas*tnas;
-  float t_usk = sqrt(s_nas1/E);
+void setTimer2Interrupt(bool enableInterrupt) {
+  if (enableInterrupt) {
+    TIMSK2 |= (1 << OCIE2A);  // включаем прерывание
+    TCNT2 = 0;                // обнуляем таймер
+  } else {
+    TIMSK2 &= ~(1 << OCIE2A);  // отключаем прерывание
+    TCNT2 = 0;                 // обнуляем таймер
+  }
+}
 
-  int num_t1 = (int)(tnas / dt) + 1;
-  int num_t2 = (int)((t_usk - tnas) / dt) + 1;
-  int num_t3 = (int)((t_usk + tnas - t_usk) / dt) + 1;
-  int total_size = num_t1 + num_t2 + num_t3;
-
-  float* t = (float*)malloc(total_size * sizeof(float));
-  float* v = (float*)malloc(total_size * sizeof(float));
-  for (int i = 0; i < num_t1; i++) {
-        t[i] = i * dt;
-        v[i] = E * t[i];
+void RampGenerator(uint32_t E, uint32_t max_speed_left, uint32_t max_speed_right, uint32_t distance_left, uint32_t distance_right) {
+  distance_right = distance_right * kob_right;
+  distance_left = distance_left * kob_left;
+  float S_left = gLeftCounter / kob_left;
+  float S_right = gRightCounter / kob_right;
+  if (given_speed_left < max_speed_left) {
+    given_speed_left += E;
+  } else {
+    float S_usk_left = gLeftCounter / kob_left;
+    given_speed_left = max_speed_left;
+    if (S_left > distance_left - 2 * S_usk_left) {
+      given_speed_left -= E;
+      if (given_speed_left < 0) {
+        given_speed_left = 0;
+      }
     }
-
-    for (int i = 0; i < num_t2; i++) {
-        t[num_t1 + i] = tnas + i * dt;
-        v[num_t1 + i] = Vmax;
+  }
+  if (given_speed_right < max_speed_right) {
+    given_speed_right += E;
+  } else {
+    float S_usk_right = gRightCounter / kob_right;
+    given_speed_right = max_speed_right;
+    if (S_right > distance_right - 2 * S_usk_right) {
+      given_speed_right -= E;
+      if (given_speed_right < 0) {
+        given_speed_right = 0;
+      }
     }
-
-    for (int i = 0; i < num_t3; i++) {
-        t[num_t1 + num_t2 + i] = t_usk + i * dt;
-        v[num_t1 + num_t2 + i] = E * (t_usk + tnas) - E * t[num_t1 + num_t2 + i];
-    }
-    return v;
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
-   Serial.begin(9600);
+  Serial.begin(9600);
 
   pinMode(LEFT_SPEED_PULSE, INPUT);
   pinMode(RIGHT_SPEED_PULSE, INPUT);
@@ -87,30 +97,36 @@ void setup() {
   Serial.flush();
   Serial.println("left");
 
-  TCCR1A = 0; // установить TCCR1A регистр в 0
+  TCCR1A = 0;  // установить TCCR1A регистр в 0
   TCCR1B = 0;
 
-    // включить прерывание Timer1 overflow:
+  // включить прерывание Timer1 overflow:
   TIMSK1 = (1 << TOIE1);
-    // Установить CS10 бит так, чтобы таймер работал при тактовой частоте:
-  TCCR1B |=  (1 << CS11);
+  // Установить CS10 бит так, чтобы таймер работал при тактовой частоте:
+  TCCR1B |= (1 << CS11);
+
+  TCCR2A = 0;
+  TCCR2B = 0;
+  // включить прерывание Timer2 overflow:
+  TIMSK2 = (1 << TOIE2);
+  // Установить CS12 и CS10 биты так, чтобы таймер работал при тактовой частоте:
+  TCCR2B |= (1 << CS22) | (1 << CS20);
 
   sei();  // включить глобальные прерывания
 }
 
 void leftPulse() {
-  gLeftCounter++;  
+  gLeftCounter++;
 }
 
 void rightPulse() {
   gRightCounter++;
 }
 
-ISR(TIMER1_OVF_vect) 
-{
- 
-  current_speed_left = gLeftCounter-prev_ticks_left; 
-  current_speed_right = gRightCounter-prev_ticks_right;
+ISR(TIMER1_OVF_vect) {
+
+  current_speed_left = gLeftCounter - prev_ticks_left;
+  current_speed_right = gRightCounter - prev_ticks_right;
 
   uint32_t error_left = given_speed_left - current_speed_left;
   uint32_t error_right = given_speed_right - current_speed_right;
@@ -120,71 +136,42 @@ ISR(TIMER1_OVF_vect)
 
   uint32_t error_i_left = error_i_left + error_left;
   uint32_t error_i_right = error_i_right + error_right;
-  
-  control_left = error_left*kp_left + error_d_left*kd_left + error_i_left*ki_left;
-  control_right = error_right*kp_right + error_d_right*kd_right + error_i_right*ki_right;
+
+  control_left = error_left * kp_left + error_d_left * kd_left + error_i_left * ki_left;
+  control_right = error_right * kp_right + error_d_right * kd_right + error_i_right * ki_right;
 
   prev_err_left = error_left;
   prev_err_right = error_right;
 
   prev_ticks_left = gLeftCounter;
   prev_ticks_right = gRightCounter;
+}
 
+ISR(TIMER2_OVF_vect) {
+  uint32_t E = 5;
+  uint32_t max_speed_left = 200;
+  uint32_t max_speed_right = 200;
+  uint32_t distance_left = 5;
+  uint32_t distance_right = 5;
+  RampGenerator(E, max_speed_left, max_speed_right, distance_left, distance_right);
 }
 
 //int speed, int distance
-void doMove(uint32_t speed_left,uint32_t speed_right,uint32_t distance_left,uint32_t distance_right)
-{
-  distance_right = distance_right*kob_right;
-  distance_left = distance_left*kob_left;
-  // gRightCounter>=distance_right && 
-  float* result = RampGenerator(10,200.0,500.0,0.01);
-  int total_size = (float*)malloc(total_size * sizeof(float));
-  for (int i = 1; i < total_size; i++) {
-        given_speed_left = result[i];
-    given_speed_right = result[i];
-    digitalWrite(LEFT_CW, HIGH);
-    digitalWrite(RIGHT_CW, LOW);
-    analogWrite(LEFT_AN, control_left); 
-    analogWrite(RIGHT_AN, prev_err_right);
-    delay(100);
-    if (result[i]<5){
-            free(result);
-            analogWrite(LEFT_AN, 0); 
-            analogWrite(RIGHT_AN, 0);
-            return 0;
-        }
-    if (Serial.available() > 1) {
-    char key = Serial.read();
-    int val = Serial.parseFloat();
-    switch (key) {
-      case 'p': kp_left = val; break;
-      case 'i': ki_left = val; break;
-      case 'd': kd_left = val; break;
-    }
-  }
+void doMove() {
+  gRightCounter = 0;
+  gLeftCounter = 0;
+  digitalWrite(LEFT_CW, HIGH);
+  digitalWrite(RIGHT_CW, LOW);
+  analogWrite(LEFT_AN, control_left);
+  analogWrite(RIGHT_AN, control_right);
   Serial.print(gLeftCounter);
   Serial.print(',');
   Serial.println(gRightCounter);
-        
-    }
-  analogWrite(LEFT_AN, 0); 
-  analogWrite(RIGHT_AN, 0);
   return 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  doMove(200,200,5,5);
-  //float* result = RampGenerator(10,200.0,500.0,0.01);
-  //int total_size = (float*)malloc(total_size * sizeof(float));
-  //for (int i = 1; i < total_size; i++) {
-       // Serial.println(result[i]);
-       // delay(100);
-        //if (result[i]==0){
-         //   free(result);
-        //}
-    //}
+  doMove();
   delay(1000);
-
 }
