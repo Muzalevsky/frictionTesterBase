e8439717df7e26650323f441177a2ba9ef5bdca0
RikOrlov <golubitskiy2013@mail.ru>
2023-12-02 02:13:15 +0300

    Update dependent_PID.ino

--- main/dependent_PID/dependent_PID.ino
+++ main/dependent_PID/dependent_PID.ino
@@ -11,40 +11,39 @@ int gRightTask = 0;
 int gLeftCounter = 0;
 int gRightCounter = 0;
 
-uint32_t prev_err_left = 0;
+int32_t prev_err_left = 0;
 uint32_t prev_ticks_left = 0;
-uint32_t error_i_left = 0;
-float control_left;
+int32_t error_i_left = 0;
+uint8_t control_left;
 uint32_t given_speed_left;
-uint32_t current_speed_left;
+int32_t current_speed_left;
 
-uint32_t prev_err_right = 0;
+int32_t prev_err_right = 0;
 uint32_t prev_ticks_right = 0;
-uint32_t error_i_right = 0;
-float control_right;
-uint32_t given_speed_right;
-uint32_t current_speed_right;
-
-uint32_t prev_err_right_slave = 0;
-uint32_t prev_ticks_right_slave = 0;
+int32_t error_i_right = 0;
 uint32_t error_i_right_slave = 0;
-float control_right_slave;
-uint32_t given_speed_right_slave;
-uint32_t current_speed_right_slave;
+uint8_t control_right;
+uint32_t given_speed_right;
+int32_t current_speed_right;
 
-float kp_left = 5.1;
-float kd_left = 0.0001;
-float ki_left = 0.000001;
+float kp_left = 3;
+float kd_left = 0;
+float ki_left = 1;
 uint32_t kob_left = 258;
 
-float kp_right = 5.1;
-float kd_right = 0.0001;
-float ki_right = 0.000001;
+float kp_right = 3;
+float kd_right = 0;
+float ki_right = 1;
 uint32_t kob_right = 261;
 
 float kp_right_slave = 5.1;
 float kd_right_slave = 0.0001;
 float ki_right_slave = 0.000001;
+
+int32_t error_left = 0;
+int32_t error_right = 0;
+int32_t error_right_slave=0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -62,7 +61,6 @@ void setup() {
   pinMode(RIGHT_CW, OUTPUT);
 
   Serial.begin(9600);
-  Serial.setTimeout(50);
   Serial.flush();
   Serial.println("left , right");
 
@@ -72,7 +70,7 @@ void setup() {
   // включить прерывание Timer1 overflow:
   TIMSK1 = (1 << TOIE1);
   // Установить CS10 бит так, чтобы таймер работал при тактовой частоте:
-  TCCR1B |= (1 << CS11);
+  TCCR1B |= (1 << CS11) | (1 << CS10);
 
   sei();  // включить глобальные прерывания
 }
@@ -90,63 +88,91 @@ ISR(TIMER1_OVF_vect) {
   current_speed_left = gLeftCounter - prev_ticks_left;
   current_speed_right = gRightCounter - prev_ticks_right;
 
-  uint32_t error_left = given_speed_right - current_speed_right;
-  uint32_t error_right = given_speed_right - current_speed_right;
-  uint32_t error_right_slave = gRightCounter - gLeftCounter;
+  error_left = given_speed_left - current_speed_left;
+  error_right = given_speed_right - current_speed_right;
+  error_right_slave = gLeftCounter - gRightCounter;
 
-  uint32_t error_d_left = error_left - prev_err_left;
-  uint32_t error_d_right = error_right - prev_err_right;
-  uint32_t error_d_right_slave = error_right_slave - prev_err_right_slave;
+  int32_t error_d_left = error_left - prev_err_left;
+  int32_t error_d_right = error_right - prev_err_right;
+  int32_t error_d_right_slave = error_right_slave - prev_err_right_slave;
+  
 
-  uint32_t error_i_left = error_i_left + error_left;
-  uint32_t error_i_right = error_i_right + error_right;
-  uint32_t error_i_right_slave = error_i_right_slave + error_right_slave;
+  error_i_left = error_i_left + error_left;
+  error_i_right = error_i_right + error_right;
+  error_i_right_slave = error_i_right_slave + error_right_slave;
 
-  control_left = error_left * kp_left + error_d_left * kd_left + error_i_left * ki_left;
-  control_right = error_right * kp_right + error_d_right * kd_right + error_i_right * ki_right;
-  control_right_slave = error_right_slave * kp_right_slave + error_d_right_slave * kd_right_slave + error_i_right_slave * ki_right_slave;
-  control_right = control_right + control_right_slave;
-
-  if (control_left > 1023) {
-    control_left = 1023;
+  if (error_i_left > 1000) {
+    error_i_left = 1000;
   }
-  if (control_right > 1023) {
-    control_right = 1023;
+  if (error_i_right > 1000) {
+    error_i_right = 1000;
+  }
+  int32_t lcontrol_left = error_left * kp_left + error_d_left * kd_left + error_i_left * ki_left;
+  int32_t lcontrol_right = error_right * kp_right + error_d_right * kd_right + error_i_right * ki_right;
+  int32_t lcontrol_right_slave = error_right_slave * kp_right_slave + error_d_right_slave * kd_right_slave + error_i_right_slave * ki_right_slave;
+  lcontrol_right = lcontrol_right + lcontrol_right_slave;
+  if (lcontrol_left <= 0) {
+    //reverse
+    digitalWrite(LEFT_CW, LOW);
+    lcontrol_left = -lcontrol_left;
+  } else {
+    digitalWrite(LEFT_CW, HIGH);
+  }
+  if (lcontrol_right <= 0) {
+    //reverse
+    lcontrol_right = -lcontrol_right;
+    digitalWrite(RIGHT_CW, HIGH);
+  } else {
+    digitalWrite(RIGHT_CW, LOW);
+  }
+
+
+  if (lcontrol_left > 255) {
+    control_left = 255;
+  } else {
+    control_left = lcontrol_left;
+  }
+  if (lcontrol_right > 255) {
+    control_right = 255;
+  } else {
+    control_right = lcontrol_right;
   }
 
 
   prev_err_left = error_left;
   prev_err_right = error_right;
-  prev_err_right_slave = error_right_slave;
 
   prev_ticks_left = gLeftCounter;
   prev_ticks_right = gRightCounter;
-  prev_ticks_right_slave = gRightCounter;
 }
 
-//int speed, int distance
 void doMove(uint32_t speed_left, uint32_t speed_right, uint32_t distance_left, uint32_t distance_right) {
   distance_right = distance_right * kob_right;
   distance_left = distance_left * kob_left;
+
   while (!(gRightCounter >= distance_right && gLeftCounter >= distance_left)) {
     given_speed_left = speed_left;
     given_speed_right = speed_right;
-    digitalWrite(LEFT_CW, HIGH);
-    digitalWrite(RIGHT_CW, LOW);
     analogWrite(LEFT_AN, control_left);
     analogWrite(RIGHT_AN, control_right);
+    Serial.print(control_left);
+    Serial.print(',');
+    Serial.println(control_right);
   }
-  Serial.print(gLeftCounter);
-  Serial.print(',');
-  Serial.println(gRightCounter);
-}
-analogWrite(LEFT_AN, 0);
-analogWrite(RIGHT_AN, 0);
+
+  analogWrite(LEFT_AN, 0);
+  analogWrite(RIGHT_AN, 0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-
-  doMove(200, 200, 10, 10);
+  doMove(50, 50, 2, 2);
+  gRightCounter = 0;
+  gLeftCounter = 0;
+  doMove(25, 25, 5, 5);
+  gRightCounter = 0;
+  gLeftCounter = 0;
+  doMove(0, 0, 10, 10);
+  gRightCounter = 0;
+  gLeftCounter = 0;
 }
